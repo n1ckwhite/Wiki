200e22756f2ce54ce40caad0c39f47d134c3ed20
Nick White <bbycinka@gmail.com>
2022-05-15 15:03:54 +0300

    feat: сброс результатов при нажатии на logo

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-const input = document.querySelector('.wiki__input');
-const form = document.querySelector('.wiki__form');
-const t = document.querySelector('.result').content;
-const list = document.querySelector('.wiki__list');
-const spinner = document.querySelector('.loader');
+const input = document.querySelector(".wiki__input");
+const form = document.querySelector(".wiki__form");
+const t = document.querySelector(".result").content;
+const list = document.querySelector(".wiki__list");
+const spinner = document.querySelector(".loader");
+const logo = document.querySelector(".logo");
 
-function createItem(title,snippet) {
-    const item = t.querySelector('.wiki__item').cloneNode(true);item.querySelector('.wiki__link').href = `https://en.wikipedia.org/wiki/${title}`;
-    item.querySelector('.wiki__item-title').textContent = title;
-    item.querySelector('.wiki__item-subtitle').textContent = `https://ru.wikipedia.org/wiki/${title}`;
-    item.querySelector('.wiki__item-descr').innerHTML = snippet;
-    return list.append(item)
+function resetResults() {
+  list.innerHTML = "";
+}
+
+function createItem(title, snippet) {
+  const item = t.querySelector(".wiki__item").cloneNode(true);
+  item.querySelector(
+    ".wiki__link"
+  ).href = `https://en.wikipedia.org/wiki/${title}`;
+  item.querySelector(".wiki__item-title").textContent = title;
+  item.querySelector(
+    ".wiki__item-subtitle"
+  ).textContent = `https://ru.wikipedia.org/wiki/${title}`;
+  item.querySelector(".wiki__item-descr").innerHTML = snippet;
+  return list.append(item);
 }
 
 function getDataForm(e) {
-    e.preventDefault();
-    list.innerHTML = '';
-    spinner.classList.add('loader_active');
-    fetch(`https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${input.value}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            spinner.classList.remove('loader_active');
-            const arr = data.query.search
-            if(arr.length > 0) {
-                arr.forEach(function(i){
-                    createItem(i.title,i.snippet)
-                });
-            } else {
-                list.textContent = 'Ничего не найдено:(';
-            }
-        })
+  e.preventDefault();
+  resetResults();
+  spinner.classList.add("loader_active");
+  fetch(
+    `https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${input.value}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      spinner.classList.remove("loader_active");
+      const arr = data.query.search;
+      if (arr.length > 0) {
+        arr.forEach(function (i) {
+          createItem(i.title, i.snippet);
+        });
+      } else {
+        list.textContent = "Ничего не найдено:(";
+      }
+    });
 }
-form.addEventListener('submit',getDataForm);
+form.addEventListener("submit", getDataForm);
+logo.addEventListener("click", resetResults);
